a46a3571776564a36ac77e7469fcab661b7222c6
Zvi Wexlstein <zvi@polar.security>
2021-06-14 11:13:05 +0300

    Bugfix

--- awsw.sh
+++ awsw.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-AWSW_OUTOUT=$(aws2-wrap --profile ${ORGANIZATION_NAME}-$1 ${@:2}  2>&1)
+aws2-wrap --profile ${ORGANIZATION_NAME}-$1 ${@:2}
 
-if [[ $? -ne 0 ]] && [[ $AWSW_OUTOUT == "Please login with "* ]]; then
+if [[ $? -ne 0 ]]; then
     aws sso login --profile=polar-security-$1
-    AWSW_OUTOUT=$(aws2-wrap --profile ${ORGANIZATION_NAME}-$1 ${@:2}  2>&1)
+    aws2-wrap --profile ${ORGANIZATION_NAME}-$1 ${@:2}
 fi
-
-echo "${AWSW_OUTOUT}"
