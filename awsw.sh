8f16f815d63a0e10731bfcc9022d340d10e7dd31
Zvi Wex <9274004+zviwex@users.noreply.github.com>
2021-09-09 15:19:44 +0300

    Update awsw.sh

--- awsw.sh
+++ awsw.sh
@@ -2,7 +2,7 @@
 
 aws2-wrap --profile ${ORGANIZATION_NAME}-$1 ${@:2}
 
-if [[ $? -ne 0 ]]; then
-    aws sso login --profile=polar-security-$1
-    aws2-wrap --profile ${ORGANIZATION_NAME}-$1 ${@:2}
-fi
+#if [[ $? -ne 0 ]]; then
+#    aws sso login --profile=polar-security-$1
+#    aws2-wrap --profile ${ORGANIZATION_NAME}-$1 ${@:2}
+#fi
