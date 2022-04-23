a7c983e770d8c369d2c71551e7222010bc6c1025
Zvi Wex <9274004+zviwex@users.noreply.github.com>
2022-04-23 03:11:35 +0300

    fix orginization name

--- awsw.sh
+++ awsw.sh
@@ -3,6 +3,6 @@
 aws2-wrap --profile ${ORGANIZATION_NAME}-$1 ${@:2}
 
 #if [[ $? -ne 0 ]]; then
-#    aws sso login --profile=polar-security-$1
+#    aws sso login --profile={ORGANIZATION_NAME}-$1
 #    aws2-wrap --profile ${ORGANIZATION_NAME}-$1 ${@:2}
 #fi
